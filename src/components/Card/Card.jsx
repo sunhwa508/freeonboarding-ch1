c911010e861ae4f646cb05d5ebfaf173f0933b62
sunhwa <2017sunhwa@gmail.com>
2021-07-27 01:14:51 +0900

    ✨[feat] card 컴포넌트 추가

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import * as Styled from "./style";
+import Loader from "../Loader/Loader";
+
+const Card = ({ data, lastElementRef, isLoading }) => {
+	return (
+		<Styled.Wrapper>
+			{isLoading
+				? new Array(10).fill(1).map((_, i) => {
+						return <Loader />;
+				  })
+				: data.map((item, index) => (
+						<Styled.CardWrapper key={item.id_index}>
+							<p ref={lastElementRef}>Comment Id {item.name}</p>
+							<p>Email {item.email}</p>
+							<p>Comment</p>
+							<p>{item.body}</p>
+						</Styled.CardWrapper>
+				  ))}
+		</Styled.Wrapper>
+	);
+};
+
+export default Card;
---
+++ src/components/Card/style.js
@@ -0,0 +1,19 @@
+import styled, { keyframes } from "styled-components";
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const CardWrapper = styled.div`
+	width: 500px;
+	height: auto;
+	background-color: #f8f9fa;
+	border: 0.5px solid #ced4da;
+	box-sizing: border-box;
+	border-radius: 20px;
+	margin-bottom: 14px;
+	padding: 20px;
+`;
