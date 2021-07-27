011a8923d704a12bcf6d685e33a90138713bfc79
sunhwa <2017sunhwa@gmail.com>
2021-07-27 16:40:27 +0900

    ♻️ refator observer 로직 변경

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import * as Styled from "./style";
-import Loader from "../Loader/Loader";
 
-const Card = ({ data, lastElementRef, isLoading }) => {
+const Card = ({ commentLists, lastElementRef }) => {
   return (
     <Styled.Wrapper>
-      {isLoading
-        ? new Array(10).fill(1).map((_, i) => {
-            return <Loader key={i} />;
-          })
-        : data.map((item, index) => (
-            <Styled.CardWrapper key={item.id + index}>
-              <div>
-                <strong ref={lastElementRef}>Comment Id</strong>
-                <span> {item.id}</span>
-              </div>
-              <div>
-                <strong>Email </strong>
-                <span>{item.email} </span>
-              </div>
-              <strong>Comment</strong>
-              <br />
-              <span>{item.body}</span>
-            </Styled.CardWrapper>
-          ))}
+      {commentLists.map(item => (
+        <Styled.CardWrapper key={item.id}>
+          <div ref={lastElementRef}>
+            <strong>Comment Id</strong>
+            <span> {item.id}</span>
+          </div>
+          <div>
+            <strong>Email </strong>
+            <span>{item.email} </span>
+          </div>
+          <strong>Comment</strong>
+          <br />
+          <span>{item.body}</span>
+        </Styled.CardWrapper>
+      ))}
     </Styled.Wrapper>
   );
 };
--- src/components/InfiniteScrollList/InfiniteScrollList.jsx
+++ src/components/InfiniteScrollList/InfiniteScrollList.jsx
@@ -1,52 +1,55 @@
-import React, { useCallback, useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../Card/Card";
 
 function InfiniteScrollList() {
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [data, setData] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
+  const [commentLists, setCommentLists] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
   const [pageNumber, setPageNumber] = useState(1);
-  const [limit, setLimit] = useState(50);
+  const loader = useRef(null);
+  const PAGE_LIMIT = 50;
 
-  const observer = useRef();
-
-  const lastElementRef = useCallback(
-      //console.log(node) 마지막 노드를 출력한다. 
-    node => {
-      if (isLoading) return;
-      if (observer.current) observer.current.disconnect();
-      //최근 observer를 갖기위해 이전 observer disconnect 해주기
-      observer.current = new IntersectionObserver(entries => {
-        // isIntersecting : 화면 노출 여부확인
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNumber(prevPageNumber => prevPageNumber + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [isLoading, hasMore],
-  );
-
-  useEffect(() => {
-    setIsLoading(true);
-    setError(false);
-
+  const getCommentList = () => {
     axios
       .get(`https://jsonplaceholder.typicode.com/comments?_page=${pageNumber}&_limit=10`)
-      .then(res => {
-        setTimeout(() => setIsLoading(false), 500);
-        setData(prevData => [...prevData, ...res.data]);
-        setHasMore(pageNumber !== limit);
+      .then(response => {
+        setIsLoading(false);
+        setCommentLists(items => [...items, ...response.data]);
+        setHasMore(pageNumber !== PAGE_LIMIT);
       })
-      .catch(err => {
-        console.warn(err);
-        setError(true);
-      });
+      .catch(error => console.warn(error));
+  };
+
+  useEffect(() => {
+    getCommentList();
   }, [pageNumber]);
 
-  return <Card data={data} isLoading={isLoading} lastElementRef={lastElementRef} />;
+  const onIntersect = entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && hasMore) {
+        setPageNumber(prev => prev + 1);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(onIntersect, options);
+    observer.observe(loader.current);
+    return () => observer.disconnect();
+  }, [loader]);
+
+  return (
+    <>
+      <Card commentLists={commentLists} />
+      <div ref={loader}>{isLoading && "loading"} </div>
+    </>
+  );
 }
 
 export default InfiniteScrollList;
